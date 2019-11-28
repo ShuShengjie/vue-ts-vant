27a65cce659b9fa4eca5c77d0a253fc1574af636
shushengjie <951341470@qq.com>
2019-11-28 10:26:49 +0800

    babel配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/app',
+    // 兼容配置
+    [
+      '@babel/preset-env',
+      {
+        'useBuiltIns': 'entry'
+      }
+    ]
+  ],
+  // 按需加载配置
+  plugins: [
   ]
 }
